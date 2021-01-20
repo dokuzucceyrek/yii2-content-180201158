467ccd872cac7b0173fb3a3b08191d17e8e601d7
Batuhan Özdemir <73344897+dokuzucceyrek@users.noreply.github.com>
2021-01-20 18:27:19 +0300

    Update DefaultController.php

--- controllers/frontend/DefaultController.php
+++ controllers/frontend/DefaultController.php
@@ -1,11 +1,11 @@
 <?php
-namespace kouosl\AirCron\controllers\frontend;
+namespace yii2-content-180201158\AirCron\controllers\frontend;
 
 
 /**
  * Default controller for the `AirCron` module
  */
-class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+class DefaultController extends \yii2-content-180201158\base\controllers\frontend\BaseController
 {
     /**
      * Renders the index view for the module
